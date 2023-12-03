04e6253a931b5ff6b6465604b7bd1f4c095f2ef0
quatarnary <contact@ilyasbugra.com>
2023-12-03 17:02:05 +0300

    break tutorial: creating-a-card-view

--- Scrumdinger/CardView.swift
+++ Scrumdinger/CardView.swift
@@ -8,11 +8,29 @@
 import SwiftUI
 
 struct CardView: View {
+    let scrum: DailyScrum
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+            Spacer()
+            HStack {
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: /*@START_MENU_TOKEN@*/"clock"/*@END_MENU_TOKEN@*/)
+                    .padding(.trailing, 20)
+            }
+            .font(.caption)
+        }
+        .padding()
     }
 }
 
-#Preview {
-    CardView()
+struct CardView_Previews: PreviewProvider {
+    static var scrum = DailyScrum.sampleData[0]
+    static var previews: some View{
+        CardView(scrum: scrum)
+            .background(scrum.theme.mainColor)
+            .previewLayout(.fixed(width: 400, height: 60))
+    }
 }
