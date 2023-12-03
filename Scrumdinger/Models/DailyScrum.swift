995e0e9da75053f93865596232bfb8237a16b6be
quatarnary <contact@ilyasbugra.com>
2023-12-03 17:00:52 +0300

    break tutorial: creating-a-card-view

---
+++ Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,8 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by Bugra Aslan on 3.12.2023.
+//
+
+import Foundation
---
+++ Scrumdinger/Models/Theme.swift
@@ -0,0 +1,8 @@
+//
+//  Theme.swift
+//  Scrumdinger
+//
+//  Created by Bugra Aslan on 2.12.2023.
+//
+
+import Foundation
