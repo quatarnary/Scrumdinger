b1609ce7af8e56b0f3a9d731326fb3090dbe8d68
quatarnary <contact@ilyasbugra.com>
2023-12-06 22:40:30 +0300

    finished tutorial: passing-data-with-bindings

---
+++ Scrumdinger/Views/ThemeView.swift
@@ -0,0 +1,25 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by Bugra Aslan on 6.12.2023.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    let theme: Theme
+    
+    var body: some View {
+        Text(theme.name)
+            .padding(4)
+            .frame(maxWidth: .infinity)
+            .background(theme.mainColor)
+            .foregroundColor(theme.accentColor)
+            .clipShape(RoundedRectangle(cornerRadius: 4))
+    }
+}
+
+#Preview {
+    ThemeView(theme: .buttercup)
+}
