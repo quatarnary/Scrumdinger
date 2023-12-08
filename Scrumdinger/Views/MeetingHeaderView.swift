390c695fc09aaccdfe67d59ee66ab8e566fc0a78
quatarnary <contact@ilyasbugra.com>
2023-12-08 18:14:13 +0300

    tutorial break, step 1 - section 5: managing-state-and-life-cycle

---
+++ Scrumdinger/Views/MeetingHeaderView.swift
@@ -0,0 +1,63 @@
+    //
+    //  MeetingHeaderView.swift
+    //  Scrumdinger
+    //
+    //  Created by Bugra Aslan on 8.12.2023.
+    //
+
+import SwiftUI
+
+struct MeetingHeaderView: View {
+    let secondsElapsed: Int
+    let secondsRemaining: Int
+    let theme: Theme
+    
+    private var totalSeconds: Int {
+        secondsElapsed + secondsRemaining
+    }
+    
+    private var progress: Double {
+        guard totalSeconds > 0 else { return 1 }
+        return Double(secondsElapsed) / Double(secondsRemaining)
+    }
+    
+    private var minutesRemaining: Int {
+        secondsRemaining / 60
+    }
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Seconds\nElapsed")
+                    .font(.caption)
+                    .multilineTextAlignment(.leading)
+                Spacer()
+                Text("Seconds\nRemaining")
+                    .font(.caption)
+                    .multilineTextAlignment(.trailing)
+            }
+            HStack {
+                VStack (alignment: .leading) {
+                    Label("\(secondsElapsed)", systemImage: "hourglass.bottomhalf.filled")
+                }
+                
+                ProgressView(value: progress)
+                    .progressViewStyle(ScrumProgressViewStyle(theme: theme))
+                    
+                VStack (alignment: .trailing) {
+                    Label("\(secondsRemaining)", systemImage: "hourglass.tophalf.filled")
+                        .labelStyle(.trailingIcon)
+                }
+            }
+        }
+        .accessibilityElement(children: .ignore)
+        .accessibilityLabel("Time Remaining")
+        .accessibilityValue("\(minutesRemaining) Minutes")
+        .padding([.top, .horizontal])
+    }
+}
+
+#Preview {
+    MeetingHeaderView(secondsElapsed: 60, secondsRemaining: 100, theme: .bubblegum)
+        .previewLayout(.sizeThatFits)
+}
--- Scrumdinger/Views/MeetingView.swift
+++ Scrumdinger/Views/MeetingView.swift
@@ -1,47 +1,41 @@
-//
-//  ContentView.swift
-//  Scrumdinger
-//
-//  Created by Bugra Aslan on 2.12.2023.
-//
+    //
+    //  ContentView.swift
+    //  Scrumdinger
+    //
+    //  Created by Bugra Aslan on 2.12.2023.
+    //
 
 import SwiftUI
 
 struct MeetingView: View {
+    @Binding var scrum: DailyScrum
+    @StateObject var scrumTimer = ScrumTimer()
+    
     var body: some View {
-        VStack {
-            HStack {
-                VStack (alignment: .leading) {
-                    Text("Seconds Elapsed")
-                        .font(.caption)
-                    Label("300", systemImage: "hourglass.bottomhalf.filled")
+        ZStack {
+            RoundedRectangle(cornerRadius: 16)
+                .fill(scrum.theme.mainColor)
+            VStack {
+                MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
+                Circle()
+                    .strokeBorder(lineWidth: 24)
+                HStack {
+                    Text("Speaker 1 of 3")
+                    Spacer()
+                    Button(action: {}){
+                        Image(systemName: "forward.fill")
+                    }
+                    .accessibilityLabel("Next Speaker")
                 }
-                Spacer()
-                VStack (alignment: .trailing) {
-                    Text("Seconds Remaining")
-                        .font(.caption)
-                    Label("300", systemImage: "hourglass.tophalf.filled")
-                }
-            }
-            .accessibilityElement(children: .ignore)
-            .accessibilityLabel("Time Remaining")
-            .accessibilityValue("10 Minutes")
-            ProgressView(value: 5, total: 15)
-            Circle()
-                .strokeBorder(lineWidth: 24)
-            HStack {
-                Text("Speaker 1 of 3")
-                Spacer()
-                Button(action: {}){
-                    Image(systemName: "forward.fill")
-                }
-                .accessibilityLabel("Next Speaker")
             }
         }
+        // .foregroundColor is deprecated, hence I used foregroundStyle
         .padding()
+        .foregroundStyle(scrum.theme.accentColor)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 #Preview {
-    MeetingView()
+    MeetingView(scrum: .constant(DailyScrum.sampleData[0]))
 }
