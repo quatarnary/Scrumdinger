0cf38d0aaa06cde7f3d39a34a85c4e5e8384e695
quatarnary <contact@ilyasbugra.com>
2023-12-08 15:45:52 +0300

    starting tutorial catch up with the starter project: managing-state-and-life-cycle

---
+++ Scrumdinger/Views/ScrumProgressViewStyle.swift
@@ -0,0 +1,36 @@
+//
+//  ScrumProgressViewStyle.swift
+//  Scrumdinger
+//
+//  Created by Bugra Aslan on 8.12.2023.
+//
+
+import SwiftUI
+
+struct ScrumProgressViewStyle: ProgressViewStyle {
+    var theme: Theme
+    
+    func makeBody(configuration: Configuration) -> some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10)
+                .fill(theme.accentColor)
+                .frame(height: 20.0)
+            if #available(iOS 15.0, *){
+                ProgressView(configuration)
+                    .tint(theme.mainColor)
+                    .frame(height: 12.0)
+                    .padding(.horizontal)
+            } else {
+                ProgressView(configuration)
+                    .frame(height: 12.0)
+                    .padding(.horizontal)
+            }
+        }
+    }
+}
+
+#Preview {
+    ProgressView(value: 0.4)
+        .progressViewStyle(ScrumProgressViewStyle(theme: .buttercup))
+        .previewLayout(.sizeThatFits)
+}
