b822ae9e8e35b06f340ed717a53e6f3533c1db0a
quatarnary <contact@ilyasbugra.com>
2023-12-11 22:37:28 +0300

    fixed the out-of-bounds progress value issue

--- Scrumdinger/Views/MeetingHeaderView.swift
+++ Scrumdinger/Views/MeetingHeaderView.swift
@@ -18,7 +18,7 @@ struct MeetingHeaderView: View {
     
     private var progress: Double {
         guard totalSeconds > 0 else { return 1 }
-        return Double(secondsElapsed) / Double(secondsRemaining)
+        return Double(secondsElapsed) / Double(totalSeconds)
     }
     
     private var minutesRemaining: Int {
