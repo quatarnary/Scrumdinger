c4970e4d4b5982cfa47cd20019a1926bf6324c19
quatarnary <contact@ilyasbugra.com>
2023-12-12 23:07:01 +0300

    section-1 finished

--- Scrumdinger/Views/MeetingView.swift
+++ Scrumdinger/Views/MeetingView.swift
@@ -20,8 +20,7 @@ struct MeetingView: View {
                 .fill(scrum.theme.mainColor)
             VStack {
                 MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
-                Circle()
-                    .strokeBorder(lineWidth: 24)
+                MeetingTimerView(speakers: scrumTimer.speakers, theme: scrum.theme)
                 MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
                     // why there is speakers in the scrumTimer??
                     // why in the hell scrumTimer also skips speakers
